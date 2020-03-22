574f8b9daa397348b6958e6ce85e3de0a51724ce
Mankianer <Marvin.woelk@web.de>
2020-03-22 11:54:11 +0100

    Edit endpoint

--- src/main/java/de/mankianer/mankisweckerservice/WeckerRestService.java
+++ src/main/java/de/mankianer/mankisweckerservice/WeckerRestService.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/wecker/")
+@RequestMapping("/api/wecker")
 public class WeckerRestService {
 
   @Autowired
